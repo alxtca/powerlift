8e48e4f2d6fb49a5183f843612518d05459efd15
Alexandr Tcacenco <alxtca@gmail.com>
2021-01-25 14:34:08 +0100

    Added workout model and relationship

--- app/models.py
+++ app/models.py
@@ -20,7 +20,45 @@ class User(UserMixin, db.Model):
 
 class Workout(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    base_move = db.Column(db.String)
+
+    main_move = db.Column(db.String(100))
+    main_move_reps = db.Column(db.Integer)
+    main_move_sets = db.Column(db.Integer)
+    main_move_rpe = db.Column(db.Integer)
+    main_move_weight = db.Column(db.Integer)
+
+    assa_move = db.Column(db.String(100))
+    assa_move_reps = db.Column(db.Integer)
+    assa_move_sets = db.Column(db.Integer)
+    assa_move_rpe = db.Column(db.Integer)
+    assa_move_weight = db.Column(db.Integer)
+
+    assb1_move = db.Column(db.String(100))
+    assb1_move_reps = db.Column(db.Integer)
+    assb1_move_sets = db.Column(db.Integer)
+    assb1_move_rpe = db.Column(db.Integer)
+    assb1_move_weight = db.Column(db.Integer)
+
+    assb2_move = db.Column(db.String(100))
+    assb2_move_reps = db.Column(db.Integer)
+    assb2_move_sets = db.Column(db.Integer)
+    assb2_move_rpe = db.Column(db.Integer)
+    assb2_move_weight = db.Column(db.Integer)
+
+    assc1_move = db.Column(db.String(100))
+    assc1_move_reps = db.Column(db.Integer)
+    assc1_move_sets = db.Column(db.Integer)
+    assc1_move_rpe = db.Column(db.Integer)
+    assc1_move_weight = db.Column(db.Integer)
+
+    assc2_move = db.Column(db.String(100))
+    assc2_move_reps = db.Column(db.Integer)
+    assc2_move_sets = db.Column(db.Integer)
+    assc2_move_rpe = db.Column(db.Integer)
+    assc2_move_weight = db.Column(db.Integer)
+
+    note = db.Column(db.String(4000))
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
